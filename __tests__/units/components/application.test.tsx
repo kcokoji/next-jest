98800b2e3813348d53fd78cfa6fdfe298b8f1158
okoji Kelechi <kelechiokoji52@gmail.com>
2024-03-28 18:06:18 +0100

    Commit error

--- __tests__/units/components/application.test.tsx
+++ __tests__/units/components/application.test.tsx
@@ -39,7 +39,7 @@ describe("Application", () => {
     expect(checkBoxElement2).toBeInTheDocument();
 
     const submitButton = screen.getByRole("button");
-    expect(submitButton).to;
+    expect(submitButton).not.toBeChecked();
     const headingTitle = screen.getByRole("heading", {
       level: 1,
     });
